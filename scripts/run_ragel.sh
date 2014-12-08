217b4f1ecc598ebb6070b8ee12405b8223ea572e
Tom <tom@universe>
2014-12-08 23:21:20 +0100

    include error origin in error message

--- scripts/run_ragel.sh
+++ scripts/run_ragel.sh
@@ -3,28 +3,29 @@
 ragel="ragel"
 ragel_file="lexer.rl"
 ragel_output="lexer.h"
+error_origin="$0"
 
 assert_dependencies()
 {
   # try to execute
   "$ragel" --help > /dev/null 2>&1 \
-    || { echo >&2 "cannot execute $ragel"; exit 1; }
+    || { echo >&2 "$error_origin: cannot execute $ragel"; exit 1; }
  
   # check if a directory was passed
   [ -d "$1" ] \
-    || { echo >&2 "project root not found"; exit 1; } 
+    || { echo >&2 "$error_origin: project root not found"; exit 1; }
 
   # check if src directory exists
   [ -d "$1/src" ] \
-    || { echo >&2 "src directory not found"; exit 1; } 
+    || { echo >&2 "$error_origin: src directory not found"; exit 1; }
   
   # check if src/hext directory exists
   [ -d "$1/src/hext" ] \
-    || { echo >&2 "hext directory not found"; exit 1; } 
+    || { echo >&2 "$error_origin: hext directory not found"; exit 1; }
   
   # check if ragel_file exists
   [ -f "$1/src/hext/$ragel_file" ] \
-    || { echo >&2 "$ragel_file not found"; exit 1; } 
+    || { echo >&2 "$error_origin: $ragel_file not found"; exit 1; }
 }
 
 print_usage()
@@ -39,7 +40,7 @@ assert_dependencies $1
 
 # change directory to have consistent #line directives (they 
 # contain the path to the file)
-cd "$1/src" || { echo >&2 "failed to cd into src directory"; exit 1; }
+cd "$1/src" || { echo >&2 "$error_origin: failed to cd into src directory"; exit 1; }
 
 path="hext/"
 options=" -o $path$ragel_output $path$ragel_file"
