c6580dd31c45192a2202709f2597c1b8157dad0a
Tom <tom@universe>
2014-12-21 15:42:23 +0100

    split builtin-captures.h into header and implementation

---
+++ src/hext/builtin-captures.cpp
@@ -0,0 +1,37 @@
+#include "hext/builtin-captures.h"
+
+
+namespace hext {
+namespace bc {
+
+
+std::string capture_inner_text(const GumboNode * node)
+{
+  assert(node != nullptr);
+  assert(node->type == GUMBO_NODE_ELEMENT);
+
+  std::string inner_text("");
+
+  const GumboVector * children = &node->v.element.children;
+  for(unsigned int i = 0; i < children->length; ++i)
+  {
+    const GumboNode * child_node =
+      static_cast<const GumboNode *>(children->data[i]);
+    assert(child_node != nullptr);
+
+    if( child_node->type == GUMBO_NODE_TEXT )
+    {
+      const GumboText * node_text = &child_node->v.text;
+      assert(node_text != nullptr);
+      assert(node_text->text != nullptr);
+      inner_text.append(node_text->text);
+    }
+  }
+
+  return inner_text;
+}
+
+
+} // namespace bc
+} // namespace hext
+
