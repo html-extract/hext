ae837bd9cc00208925bef6857039359fddff663e
Tom <tom@universe>
2014-12-26 13:40:27 +0100

    removed redundant getters, setters and typedefs

--- src/hext/match-tree.cpp
+++ src/hext/match-tree.cpp
@@ -21,38 +21,14 @@ void match_tree::append_match(const name_value_pair& p)
   this->matches.push_back(p);
 }
 
-match_tree::const_child_iterator match_tree::children_begin() const
-{
-  return this->children.begin();
-}
-
-match_tree::const_child_iterator match_tree::children_end() const
-{
-  return this->children.end();
-}
-
-std::vector<match_tree>::size_type match_tree::children_size() const
-{
-  return this->children.size();
-}
-
-match_tree::const_match_iterator match_tree::matches_begin() const
-{
-  return this->matches.begin();
-}
-
-match_tree::const_match_iterator match_tree::matches_end() const
-{
-  return this->matches.end();
-}
-
 void match_tree::to_json(std::ostream& out) const
 {
   if( !this->matches.empty() )
   {
     out << "{";
     assert(this->matches.size() > 0);
-    match_tree::const_match_iterator it_last = this->matches.end() - 1;
+    std::vector<match_tree::name_value_pair>::const_iterator
+      it_last = this->matches.end() - 1;
     for(auto it = this->matches.begin(); it != this->matches.end(); ++it)
     {
       out << it->first << ": \""
--- src/hext/match-tree.h
+++ src/hext/match-tree.h
@@ -18,28 +18,14 @@ class match_tree
 {
 public:
   typedef
-    std::vector<std::unique_ptr<match_tree>>::const_iterator
-    const_child_iterator;
-  typedef
     std::pair<std::string, std::string>
     name_value_pair;
-  typedef
-    std::vector<std::pair<std::string, std::string>>::const_iterator
-    const_match_iterator;
 
   match_tree();
 
   match_tree * append_child_and_own(std::unique_ptr<match_tree> m);
 
   void append_match(const name_value_pair& p);
-
-  const_child_iterator children_begin() const;
-  const_child_iterator children_end() const;
-
-  std::vector<match_tree>::size_type children_size() const;
-
-  const_match_iterator matches_begin() const;
-  const_match_iterator matches_end() const;
 
   void to_json(std::ostream& out = std::cout) const;
 
