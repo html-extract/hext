6c5318f30114c193c62974a34cd96883d43428ee
Tom <tom@universe>
2015-01-03 16:40:01 +0100

    reorder methods in match-tree.cpp to match ordering in match-tree.h

--- src/hext/match-tree.cpp
+++ src/hext/match-tree.cpp
@@ -23,6 +23,11 @@ void match_tree::append_match(const name_value_pair& p)
   this->matches.push_back(p);
 }
 
+void match_tree::set_rule(const rule * matching_rule)
+{
+  this->r = matching_rule;
+}
+
 void match_tree::print_json(std::ostream& out) const
 {
   for(const auto& c : this->children)
@@ -35,72 +40,12 @@ void match_tree::print_json(std::ostream& out) const
   }
 }
 
-void
-match_tree::print_json_matches(
-  infix_ostream_iterator<std::string>& out
-) const
-{
-  for(const auto& p : this->matches)
-  {
-    std::string str("\"");
-    str.append(util::escape_quotes(p.first))
-       .append("\": \"")
-       .append(util::escape_quotes(p.second))
-       .append("\"");
-    out = str;
-  }
-}
-
-void
-match_tree::print_json_recursive(
-  infix_ostream_iterator<std::string>& out
-) const
-{
-  this->print_json_matches(out);
-
-  for(const auto& c : this->children)
-    c->print_json_recursive(out);
-}
-
 void match_tree::print_dot(std::ostream& out) const
 {
   out << "digraph match_tree {\n"
       << "    node [fontname=\"Arial\"];\n";
   this->print_dot_nodes(out);
   out << "}\n";
-}
-
-void match_tree::print_dot_nodes(std::ostream& out, int parent_id) const
-{
-  static int node_index = 0;
-  int this_node = ++node_index;
-
-  std::string label;
-  if( this->r == nullptr || this->r->get_tag_name().empty() )
-    label.append("[rule]");
-  else
-    label.append(this->r->get_tag_name());
-
-  for(const auto& m : this->matches)
-  {
-    label.append(" ");
-    label.append(m.first);
-  }
-
-  if( this->r && this->r->children_size() == 0 )
-    label.append("*");
-
-  out << "    node_" << this_node << " [label=\"" << label << "\"];\n";
-  if( parent_id )
-    out << "    node_" << parent_id << " -> node_" << this_node << ";\n";
-
-  for(const auto& c : this->children)
-    c->print_dot_nodes(out, this_node);
-}
-
-void match_tree::set_rule(const rule * matching_rule)
-{
-  this->r = matching_rule;
 }
 
 bool match_tree::filter()
@@ -142,6 +87,59 @@ bool match_tree::filter()
   // TODO: can this break?
   else
     return this->r->children_size() > this->children.size();
+}
+
+void match_tree::print_json_recursive(
+  infix_ostream_iterator<std::string>& out
+) const
+{
+  this->print_json_matches(out);
+
+  for(const auto& c : this->children)
+    c->print_json_recursive(out);
+}
+
+void match_tree::print_json_matches(
+  infix_ostream_iterator<std::string>& out
+) const
+{
+  for(const auto& p : this->matches)
+  {
+    std::string str("\"");
+    str.append(util::escape_quotes(p.first))
+       .append("\": \"")
+       .append(util::escape_quotes(p.second))
+       .append("\"");
+    out = str;
+  }
+}
+
+void match_tree::print_dot_nodes(std::ostream& out, int parent_id) const
+{
+  static int node_index = 0;
+  int this_node = ++node_index;
+
+  std::string label;
+  if( this->r == nullptr || this->r->get_tag_name().empty() )
+    label.append("[rule]");
+  else
+    label.append(this->r->get_tag_name());
+
+  for(const auto& m : this->matches)
+  {
+    label.append(" ");
+    label.append(m.first);
+  }
+
+  if( this->r && this->r->children_size() == 0 )
+    label.append("*");
+
+  out << "    node_" << this_node << " [label=\"" << label << "\"];\n";
+  if( parent_id )
+    out << "    node_" << parent_id << " -> node_" << this_node << ";\n";
+
+  for(const auto& c : this->children)
+    c->print_dot_nodes(out, this_node);
 }
 
 
