db9981a13e6a585e11cf7cf82b316bc54da07a11
Tom <tom@universe>
2015-01-03 21:01:06 +0100

    added documentation to class attribute

--- src/hext/attribute.h
+++ src/hext/attribute.h
@@ -12,6 +12,11 @@
 namespace hext {
 
 
+/// An attribute is a key-value pair of a rule.
+/// An attribute may be
+/// * a builtin attribute (e.g. hext-inner-text="(:title)"),
+/// * a capture attribute (e.g. class="(:class_name)"), or
+/// * a literal attribute (e.g. id="container")
 class attribute
 {
 public:
@@ -23,8 +28,12 @@ public:
   void set_name(const std::string& attribute_name);
   void set_value(const std::string& attribute_value);
 
+  /// Check if a GumboNode can satisfy this attribute.
+  /// Returns true on match.
   bool matches(const GumboNode * node) const;
 
+  /// Extract a name_value_pair described by this attribute from a GumboNode.
+  /// Returns empty value if there is no match.
   match_tree::name_value_pair capture(const GumboNode * node) const;
 
   void print(std::ostream& out = std::cout) const;
