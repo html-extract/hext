bb5728574e565a7dac40da800bc8b59298828124
Tom <tom@universe>
2015-01-03 21:59:31 +0100

    added documentation to parser

--- src/hext/parser.h
+++ src/hext/parser.h
@@ -26,8 +26,10 @@ public:
 };
 
 
+/// Use lexer to lex from begin to end, then convert tokens to vector<rule>.
 std::vector<rule> parse_range(const char * begin, const char * end);
 
+/// Read file into buffer and calls parser::parse_range.
 std::vector<rule> parse_file(const std::string& path);
 
 
