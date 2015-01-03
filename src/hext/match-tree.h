634027f7c3488513b931801082f36cd6f4442587
Tom <tom@universe>
2015-01-03 16:34:54 +0100

    declare helper functions private

--- src/hext/match-tree.h
+++ src/hext/match-tree.h
@@ -28,23 +28,21 @@ public:
   match_tree();
 
   match_tree * append_child_and_own(std::unique_ptr<match_tree> m);
-
   void append_match(const name_value_pair& p);
+  void set_rule(const rule * matching_rule);
 
   void print_json(std::ostream& out = std::cout) const;
-  void print_json_recursive(infix_ostream_iterator<std::string>& out) const;
-  void print_json_matches(infix_ostream_iterator<std::string>& out) const;
-
   void print_dot(std::ostream& out = std::cout) const;
-  void print_dot_nodes(std::ostream& out, int parent_id = 0) const;
-
-  void set_rule(const rule * matching_rule);
 
   bool filter();
 
 private:
   match_tree(const match_tree&) = delete;
   match_tree& operator=(const match_tree&) = delete;
+
+  void print_json_recursive(infix_ostream_iterator<std::string>& out) const;
+  void print_json_matches(infix_ostream_iterator<std::string>& out) const;
+  void print_dot_nodes(std::ostream& out, int parent_id = 0) const;
 
   std::vector<std::unique_ptr<match_tree>> children;
   std::vector<name_value_pair> matches;
