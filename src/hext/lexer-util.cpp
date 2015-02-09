470a4faad04bdc735a211236189ee343dbfd9735
Tom <tom@universe>
2015-02-09 23:38:49 +0100

    when calling isprint explicitly use std namespace

--- src/hext/lexer-util.cpp
+++ src/hext/lexer-util.cpp
@@ -20,7 +20,7 @@ std::string get_char_name(char c)
       return "[nullbyte]";
     default:
       int ci = c;
-      return isprint(ci) ?
+      return std::isprint(ci) ?
           // std::string has no constructor accepting char; abusing fill-constructor
           std::string(1, c)
         : std::string("[ascii: ") + std::to_string(ci) + "]";
