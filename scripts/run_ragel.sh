c5d8d25350688a1b5086eb32d157623a840bc94f
Tom <tom@universe>
2015-02-19 13:54:29 +0100

    always echo the script's name, even on success

--- scripts/run_ragel.sh
+++ scripts/run_ragel.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 ragel="ragel"
-error_origin="$0"
+script_name="$0"
 
 assert_dependencies()
 {
   # try to execute
   "$ragel" --help > /dev/null 2>&1 \
-    || { echo >&2 "$error_origin: cannot execute $ragel"; exit 1; }
+    || { echo >&2 "$script_name: cannot execute $ragel"; exit 1; }
  
   # check if input-file exists
   [ -f "$1" ] \
-    || { echo >&2 "$error_origin: $1 not found"; exit 1; }
+    || { echo >&2 "$script_name: $1 not found"; exit 1; }
 }
 
 print_usage()
 {
-  echo "Usage: $error_origin [ragel-input-file.rl] [output-file]"
+  echo "Usage: $script_name [ragel-input-file.rl] [output-file]"
   echo "       Wrapper for ragel"
 }
 
@@ -31,9 +31,9 @@ ragel_output="$2"
 ragel_exit=$?
 
 if [ $ragel_exit -eq 0 ] ; then
-  echo "Generated '$ragel_output'"
+  echo "$script_name: Generated '$ragel_output'"
 else
-  echo >&2 "$error_origin: ragel failed"
+  echo >&2 "$script_name: ragel failed"
 fi
 
 exit $ragel_exit
