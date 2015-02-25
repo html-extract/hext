be06a75c5ae005532ceff7e5bada9ebd0c138162
Tom <tom@universe>
2015-02-25 23:44:18 +0100

    add comments to ValueTest

--- src/hext/pattern/value-test.h
+++ src/hext/pattern/value-test.h
@@ -8,16 +8,23 @@
 namespace hext {
 
 
+/// ValueTest is the base class for all pattern tests. Pattern tests are used
+/// to validate attribute values of nodes according to a Rule's definition.
+/// In its basic form it considers any value as valid.
+/// See also LiteralTest and RegexTest.
 class ValueTest
 {
 public:
   virtual ~ValueTest();
+  /// Always return true.
   virtual bool test(const char * /* dummy */) const;
+  /// Print nothing.
   virtual void print(std::ostream& /* dummy */ = std::cout) const;
 };
 
 
 } // namespace hext
+
 
 #endif // HEXT_VALUE_TEST_H
 
