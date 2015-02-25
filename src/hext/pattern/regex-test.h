e2aa5792c3e6fd245531085abfcf25d9b3037e58
Tom <tom@universe>
2015-02-25 23:44:49 +0100

    add comments to RegexTest

--- src/hext/pattern/regex-test.h
+++ src/hext/pattern/regex-test.h
@@ -12,10 +12,13 @@
 namespace hext {
 
 
+/// A RegexTest is a ValueTest that checks whether another string matches a
+/// given regex.
 class RegexTest : public ValueTest
 {
 public:
   explicit RegexTest(const std::string& regex);
+  /// Returns true if regex matches subject.
   bool test(const char * subject) const final;
   void print(std::ostream& out = std::cout) const final;
 
