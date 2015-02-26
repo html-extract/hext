570a7cc7025522ce861574ec7f0ff88fcf6ca4e3
Tom <tom@universe>
2015-02-26 02:38:08 +0100

    add comments to builtins.h

--- src/hext/builtins.h
+++ src/hext/builtins.h
@@ -12,9 +12,13 @@
 namespace hext {
 
 
+/// Convenience typedef for function pointers pointing to so called builtin
+/// functions (like @text)
 typedef std::string (*BuiltinFuncPtr)(const GumboNode *);
 
+/// Return a function pointer to a builtin called builtin_name.
 BuiltinFuncPtr GetBuiltinByName(const std::string& builtin_name);
+/// Return the name of the builtin which is pointed to by f.
 std::string GetNameByBuiltin(BuiltinFuncPtr f);
 
 
