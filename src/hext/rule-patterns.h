ba86d7c1589b3b975ea2223134d1f738a2be566e
Tom <tom@universe>
2015-02-26 02:42:17 +0100

    add comments to rule-patterns.h

--- src/hext/rule-patterns.h
+++ src/hext/rule-patterns.h
@@ -18,6 +18,7 @@ namespace hext {
 
 
 /// RulePatterns contain the patterns of a rule.
+/// RulePatterns were created to lessen the amount of code in Rule.
 /// There are two types of patterns: MatchPatterns and CapturePatterns.
 class RulePatterns
 {
@@ -29,13 +30,14 @@ public:
   ~RulePatterns();
   RulePatterns(RulePatterns&&) = default;
   /// Apply all MatchPatterns to node.
-  /// Returns true if all MatchPatterns match.
+  /// Return true if all MatchPatterns match.
   bool matches(const GumboNode * node) const;
   /// Apply all MatchPatterns to node.
-  /// Returns true if all node-attributes were matched.
+  /// Return true if all MatchPatterns match and each node-attribute was
+  /// matched with a MatchPattern ("closed Rule").
   bool matches_all_attributes(const GumboNode * node) const;
   /// Apply all capture patterns to node.
-  /// Returns a new MatchTree branch.
+  /// Return a new MatchTree branch containing name-value-pair.
   std::unique_ptr<MatchTree> capture(const GumboNode * node) const;
   void print(std::ostream& out = std::cout) const;
 
