7d2d8acac12130d0c59ecdee4720632d0ad7f02e
Tom <tom@universe>
2015-02-26 02:41:42 +0100

    add comments to program-options.h

--- src/hext/program-options.h
+++ src/hext/program-options.h
@@ -10,18 +10,20 @@
 namespace hext {
 
 
+// The project's program options.
 class ProgramOptions
 {
 public:
   ProgramOptions();
   ~ProgramOptions();
 
+  /// Store argv and validate or throw boost::program_options::error.
   void store(int argc, const char * argv[]);
-
+  /// Check if key was given as an argument.
   bool contains(const char * key) const;
-
+  /// Get value from parameter.
   std::string get(const char * key) const;
-
+  /// Print usage and options.
   void print(const char * program_name, std::ostream& out = std::cout) const;
 
 private:
