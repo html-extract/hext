9914051eeb0ccef43bb6ff1c8c867e679c0a4c0c
Tom <tom@universe>
2015-03-04 03:48:09 +0100

    don't throw std::runtime_error if gumbo returns nullptr

--- src/hext/html.cpp
+++ src/hext/html.cpp
@@ -7,21 +7,19 @@ namespace hext {
 Html::Html(const char * buffer, std::size_t length)
 : g_outp_(gumbo_parse_with_options(&kGumboDefaultOptions, buffer, length))
 {
-  if( !this->g_outp_ )
-    throw std::runtime_error("html parsing failed; gumbo_parse returned nullptr");
 }
 
 Html::~Html()
 {
-  assert(this->g_outp_ != nullptr);
-  gumbo_destroy_output(&kGumboDefaultOptions, this->g_outp_);
+  if( this->g_outp_ )
+    gumbo_destroy_output(&kGumboDefaultOptions, this->g_outp_);
 }
 
 std::unique_ptr<ResultTree> Html::extract(const Rule& r) const
 {
-  assert(this->g_outp_ != nullptr);
   std::unique_ptr<ResultTree> rt = MakeUnique<ResultTree>();
-  r.extract(this->g_outp_->root, rt.get());
+  if( this->g_outp_ )
+    r.extract(this->g_outp_->root, rt.get());
   return rt;
 }
 
--- src/hext/html.h
+++ src/hext/html.h
@@ -7,7 +7,6 @@
 
 #include <cassert>
 #include <string>
-#include <stdexcept>
 #include <memory>
 
 #include <gumbo.h>
