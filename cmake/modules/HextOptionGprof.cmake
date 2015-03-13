fbf7baebe61af426f171a62f2bd96768ccf6d6f4
Tom <tom@universe>
2015-03-13 18:47:34 +0100

    move option GPROF into HextOptionGprof.cmake

---
+++ cmake/modules/HextOptionGprof.cmake
@@ -0,0 +1,9 @@
+# Option GPROF, default OFF
+# GNU gprof build
+
+OPTION(GPROF "gprof build" OFF)
+IF(GPROF)
+  SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg") 
+  SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pg")
+ENDIF(GPROF)
+
