7400439ef8ab1012a04513d12cbd9b4a70e60626
Tom <tom@universe>
2015-03-14 17:00:34 +0100

    move external project gumbo-parser into HextExternalProjectGumbo.cmake

---
+++ cmake/modules/HextExternalProjectGumbo.cmake
@@ -0,0 +1,24 @@
+EXTERNALPROJECT_ADD(
+    gumbo-parser
+    GIT_REPOSITORY https://github.com/google/gumbo-parser.git
+    TIMEOUT 10
+
+    CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure --prefix=${CMAKE_BINARY_DIR}
+    BUILD_COMMAND make
+    UPDATE_COMMAND ""
+    INSTALL_COMMAND ""
+
+    BUILD_IN_SOURCE ON
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON)
+
+# use gumbo's include/ directory as an additional include directory
+EXTERNALPROJECT_GET_PROPERTY(gumbo-parser source_dir)
+INCLUDE_DIRECTORIES(${source_dir}/src)
+
+# add gumbo's library directory when linking
+EXTERNALPROJECT_GET_PROPERTY(gumbo-parser binary_dir)
+# LINK_DIRECTORIES will apply only to targets created after it is called
+LINK_DIRECTORIES(${binary_dir}/.libs)
+
