0f2c48a01abd62ce557ff17dd9e7598a6ae94ebc
Tom <tom@universe>
2015-03-17 06:00:59 +0100

    improve commentary

--- libhext/include/hext/Builtins.h
+++ libhext/include/hext/Builtins.h
@@ -12,13 +12,16 @@
 namespace hext {
 
 
-/// Convenience typedef for function pointers pointing to so called builtin
-/// functions (like @text)
+/// Convenience typedef for function pointers pointing to builtin
+/// functions (like @text).
 typedef std::string (*BuiltinFuncPtr)(const GumboNode *);
 
 /// Return a function pointer to a builtin called builtin_name.
+/// Return nullptr for unknown builtin.
 BuiltinFuncPtr GetBuiltinByName(const std::string& builtin_name);
+
 /// Return the name of the builtin which is pointed to by f.
+/// Return the string "unknown-builtin" for unknown builtin.
 std::string GetNameByBuiltin(BuiltinFuncPtr f);
 
 
