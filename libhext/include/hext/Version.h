964afaba57f147200e63de344e34b16c3b4783da
Tom <tom@universe>
2015-03-19 02:03:11 +0100

    add version information to libhext

---
+++ libhext/include/hext/Version.h
@@ -0,0 +1,24 @@
+#ifndef HEXT_VERSION_H_INCLUDED
+#define HEXT_VERSION_H_INCLUDED
+
+#include <iostream>
+
+
+namespace hext {
+
+
+/// Major version numer.
+extern const int version_major;
+
+/// Minor version number.
+extern const int version_minor;
+
+/// Git SHA-1 of this build.
+extern const char * version_git_sha1;
+
+
+} // namespace hext
+
+
+#endif // HEXT_VERSION_H_INCLUDED
+
