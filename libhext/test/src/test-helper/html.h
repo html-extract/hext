f494dab44a7a0a89b818a1d4c074525b8cf45521
Tom <tom@universe>
2015-03-22 17:53:33 +0100

    if <body> cannot be found, print error message to stderr

--- libhext/test/src/test-helper/html.h
+++ libhext/test/src/test-helper/html.h
@@ -36,21 +36,35 @@ public:
   {
     const GumboNode * node = this->g_outp_->root;
     if( !node || node->type != GUMBO_NODE_ELEMENT )
+    {
+      std::cerr << "helper::Html:" << __LINE__ << ": gumbo returned null\n";
       return nullptr;
+    }
 
     const GumboVector& children = node->v.element.children;
 
     // first child should be <head>, second should be <body> (if they are not
     // present in the input, gumbo will insert them).
     if( children.length < 2 )
+    {
+      std::cerr << "helper::Html:" << __LINE__ << ": children.length < 2\n";
       return nullptr;
+    }
     node = static_cast<const GumboNode *>(children.data[1]);
 
     if( !node || node->type != GUMBO_NODE_ELEMENT )
+    {
+      std::cerr << "helper::Html:" << __LINE__
+                << ": expected body is not a GUMBO_NODE_ELEMENT\n";
       return nullptr;
+    }
 
     if( node->v.element.tag != GUMBO_TAG_BODY )
+    {
+      std::cerr << "helper::Html:" << __LINE__
+                << ": expected body not having tag GUMBO_TAG_BODY\n";
       return nullptr;
+    }
 
     return node;
   }
