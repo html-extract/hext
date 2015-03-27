8e468787082c8b9ae48a782b24ebcf72a0ea55b7
Tom <tom@universe>
2015-03-27 08:01:39 +0100

    fix ContainsWord not finding words after an invalid match

--- libhext/src/pattern/ContainsWordTest.cpp
+++ libhext/src/pattern/ContainsWordTest.cpp
@@ -15,19 +15,25 @@ bool ContainsWordTest::test(const char * subject) const
     return false;
 
   auto str_subject = std::string(subject);
-  auto begin = str_subject.find(this->lit_);
 
-  if( begin == std::string::npos )
-    return false;
+  std::string::size_type begin = 0;
+  while( (begin = str_subject.find(this->lit_, begin)) != std::string::npos )
+  {
+    std::string::size_type end = begin + this->lit_.size();
 
-  std::string::size_type end = begin + this->lit_.size();
+    // Check if either at start of string or prefixed with space.
+    bool start_is_bound = ( begin == 0 || str_subject[begin-1] == ' ' );
 
-  // Check if either at start of string or prefixed with space.
-  bool start_is_bound = ( begin == 0 || str_subject[begin-1] == ' ' );
-  // Check if either at end of string or suffixed with space.
-  bool end_is_bound = ( end == str_subject.size() || str_subject[end] == ' ' );
+    // Check if either at end of string or suffixed with space.
+    bool end_is_bound = ( end == str_subject.size() || str_subject[end] == ' ' );
 
-  return start_is_bound && end_is_bound;
+    if( start_is_bound && end_is_bound )
+      return true;
+
+    begin = end;
+  }
+
+  return false;
 }
 
 
