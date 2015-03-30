fdb4c0e3a2c0878cfdb52d4bf469293ea63d9e03
Tom <tom@universe>
2015-03-30 20:21:26 +0200

    improve NodeUtil documentation

--- libhext/include/hext/NodeUtil.h
+++ libhext/include/hext/NodeUtil.h
@@ -14,36 +14,38 @@
 namespace hext {
 
 
-/// Get the position of node within its parent. Only counts nodes of type
-/// GUMBO_NODE_ELEMENT. First node has position 1. Returns 0 if no parent.
-/// If count_tag is set to GUMBO_TAG_UNKNOWN all nodes are counted, regardless
-/// of their tag name. If any other gumbo tag is passed, count only nodes that
-/// have tag equal to count_tag.
+/// Get the position of `node` within its parent. Only count nodes of type
+/// `GUMBO_NODE_ELEMENT`. First node has position 1. Return 0 if no parent.
+/// If `count_tag` is set to `GUMBO_TAG_UNKNOWN` all nodes are counted,
+/// regardless of their tag name. If any other `GumboTag` is passed, count
+/// only nodes that have a tag equal to `count_tag`.
 unsigned int GetNodePositionWithinParent(
   const GumboNode * node,
   GumboTag count_tag = GUMBO_TAG_UNKNOWN
 );
 
-/// Get the position of node within its parent in reverse. Only counts nodes of
-/// type GUMBO_NODE_ELEMENT. Last node has position 1. Returns 0 if no parent.
-/// If count_tag is set to GUMBO_TAG_UNKNOWN all nodes are counted, regardless
-/// of their tag name. If any other gumbo tag is passed, count only nodes that
-/// have tag equal to count_tag.
+/// Get the position of `node` within its parent in reverse. Only count nodes
+/// of type `GUMBO_NODE_ELEMENT`. Last node has position 1. Return 0 if no
+/// parent. If `count_tag` is set to `GUMBO_TAG_UNKNOWN` all nodes are counted,
+/// regardless of their tag name. If any other `GumboTag` is passed, count only
+/// nodes that have tag equal to `count_tag`.
 unsigned int GetNodePositionWithinParentReverse(
   const GumboNode * node,
   GumboTag count_tag = GUMBO_TAG_UNKNOWN
 );
 
-/// Count amount of children. Only count children of type GUMBO_NODE_ELEMENT.
+/// Count amount of children with node type `GUMBO_NODE_ELEMENT` (which excludes
+/// text-nodes).
 unsigned int GetNodeChildCount(const GumboNode * node);
 
-/// Get node's inner text. Uses hext::trim_and_collapse_ws.
+/// Get inner text of a `node`. Trim left and right whitespace and collapse
+/// multiple whitespace into a single space.
 std::string GetNodeText(const GumboNode * node);
 
-/// Get node's inner text, as is.
+/// Get inner text of a `node`, as is, untouched.
 std::string GetNodeRawText(const GumboNode * node);
 
-/// Get node's inner html as an untouched copy of the source buffer.
+/// Get inner html of a `node`, as is, untouched.
 std::string GetNodeInnerHtml(const GumboNode * node);
 
 
