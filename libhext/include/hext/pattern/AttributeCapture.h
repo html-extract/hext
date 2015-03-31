031d365313738750e95c5e4170d4561f0f343f22
Tom <tom@universe>
2015-03-31 21:58:06 +0200

    improve AttributeCapture documentation

--- libhext/include/hext/pattern/AttributeCapture.h
+++ libhext/include/hext/pattern/AttributeCapture.h
@@ -12,20 +12,41 @@
 namespace hext {
 
 
-/// An AttributeCapture is a CapturePattern that is used to capture whole
-/// attribute values from html-nodes.
+/// An AttributeCapture is a CapturePattern that extracts an html-attribute
+/// from an html-node.
 class AttributeCapture : public CapturePattern
 {
 public:
+
+  /// Construct an AttributeCapture.
+  ///
+  /// \param result_name
+  ///     The name of the capture.
+  /// \param attr_name
+  ///     The name of the attribute whose value should be captured.
   AttributeCapture(
     const std::string& result_name,
     const std::string& attr_name
   );
+
+  /// Construct an AttributeCapture that has a regex. See class CapturePattern
+  /// for an explanation on how the regex is applied to the attribute-value.
+  ///
+  /// \param result_name
+  ///     The name of the capture.
+  /// \param attr_name
+  ///     The name of the attribute whose value should be captured.
   AttributeCapture(
     const std::string& result_name,
     const std::string& attr_name,
     const boost::regex& regex
   );
+
+  /// Return a string pair containing the captured value. First member is
+  /// `result_name`, second member is the node-attribute of `node` called
+  /// `attr_name`. If the attribute isn't found, an empty string is used
+  /// instead. If a regex was given, it will be applied to attribute-value,
+  /// capturing only the desired result.
   ResultTree::NameValuePair capture(const GumboNode * node) const final;
 
 private:
