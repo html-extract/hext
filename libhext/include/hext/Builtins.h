9699571459f8b56256e092e624810b0803ef354f
Tom <tom@universe>
2015-06-21 15:35:46 +0200

    remove Builtins.cpp, move StripTagsWrapper into header

--- libhext/include/hext/Builtins.h
+++ libhext/include/hext/Builtins.h
@@ -3,7 +3,6 @@
 
 #include "hext/NodeUtil.h"
 
-#include <cassert>
 #include <string>
 
 #include <gumbo.h>
@@ -16,13 +15,10 @@ namespace hext {
 /// functions (like `@text`).
 typedef std::string (*BuiltinFuncPtr)(const GumboNode *);
 
-/// Return a function pointer to a builtin called `builtin_name`.
-/// Return nullptr for unknown builtin.
-BuiltinFuncPtr GetBuiltinByName(const std::string& builtin_name);
-
 /// A wrapper for hext::StripTags with disabled smart_wrap. Builtin functions
-/// can only have a single paramter (node).
-std::string StripTagsWrapper(const GumboNode * node);
+/// can only have a single parameter (node).
+inline std::string StripTagsWrapper(const GumboNode * node)
+  { return StripTags(node, /* smart_wrap: */ false); }
 
 
 } // namespace hext
