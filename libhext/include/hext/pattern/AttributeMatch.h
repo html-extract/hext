a2068282f3b8233d9d07f5ba8bdb28a99d7bfe2d
Tom <tom@universe>
2015-06-23 09:52:59 +0200

    AttributeMatch: add default value for value_test

--- libhext/include/hext/pattern/AttributeMatch.h
+++ libhext/include/hext/pattern/AttributeMatch.h
@@ -1,7 +1,9 @@
 #ifndef HEXT_PATTERN_ATTRIBUTE_MATCH_H_INCLUDED
 #define HEXT_PATTERN_ATTRIBUTE_MATCH_H_INCLUDED
 
+#include "hext/MakeUnique.h"
 #include "hext/test/ValueTest.h"
+#include "hext/test/NotNull.h"
 #include "hext/pattern/ValueMatch.h"
 
 #include <string>
@@ -26,7 +28,7 @@ public:
   ///     The ValueTest which is to be applied to a node's attribute value.
   AttributeMatch(
     std::string attr_name,
-    std::unique_ptr<test::ValueTest> value_test
+    std::unique_ptr<test::ValueTest> value_test = MakeUnique<test::NotNull>()
   );
 
   /// Return true if `node` has an attribute called `attr_name` whose value is
