7498ac86598816656e86e15601238f4a5bf1d405
Tom <tom@universe>
2015-06-30 18:27:33 +0200

    add examples/ to libhext

---
+++ libhext/examples/chaining.cpp
@@ -0,0 +1,74 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "hext/Hext.h"
+
+int main()
+{
+  using namespace hext;
+
+  Rule rdiv;
+  rdiv.set_tag(GUMBO_TAG_DIV)
+  // <div>
+      .add_match<AttributeCountMatch>(0)
+      .take_child(std::move(
+  //    <ul>
+        Rule(GUMBO_TAG_UL)
+  //      :attribute-count(0)
+          .add_match<AttributeCountMatch>(0)
+          .take_child(std::move(
+  //        <li>
+            Rule(GUMBO_TAG_LI)
+              .take_child(std::move(
+  //            <a>
+                Rule(GUMBO_TAG_A)
+  //               href
+                  .add_match<AttributeMatch>("href")
+  //               href={link}
+                  .add_capture<AttributeCapture>("link", "href")
+              ))
+          ))
+      ))
+  ;
+
+  Html html(R"input(
+    <html>
+      <head></head>
+      <body>
+        <div>
+          <ul attribute>
+            <li><a href="nope1"></a></li>
+            <li><a href="nope2"></a></li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <li><a href="result1"></a></li>
+            <li><a href="result2"></a></li>
+            <li><a href="result3"></a></li>
+            <li><a href="result4"></a></li>
+            <li><a href="result5"></a></li>
+            <li><a href="result6"></a></li>
+            <li><a href="result7"></a></li>
+            <li><a nope3></a></li>
+          </ul>
+          <div>
+            <div>
+              <a href="nope4"></a>
+            </div>
+          </div>
+        </div>
+      </body>
+    </html>
+  )input");
+
+  auto result_tree = rdiv.extract(html.root());
+  auto v = result_tree->flatten();
+
+  for(const auto& m : v)
+    for(const auto& p : m)
+      std::cout << p.first << ": " << p.second << "\n";
+
+  return EXIT_SUCCESS;
+}
+
---
+++ libhext/examples/extract-href.cpp
@@ -0,0 +1,44 @@
+#include "hext/Hext.h"
+
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  using namespace hext;
+
+  Rule a_href(GUMBO_TAG_A);
+  a_href.add_match<AttributeMatch>("href")
+        .add_match<AttributeMatch>("href",
+          MakeUnique<test::Negate>(
+            MakeUnique<test::BeginsWith>("javascript:")
+          )
+        )
+        .add_match<AttributeMatch>("href",
+          MakeUnique<test::Negate>(
+            MakeUnique<test::BeginsWith>("#")
+          )
+        )
+        .add_capture<AttributeCapture>("link", "href");
+
+  std::stringstream in_sstr;
+  {
+    std::ifstream in("/dev/stdin");
+    in_sstr << in.rdbuf();
+  }
+
+  Html html(in_sstr.str());
+  auto result_tree = a_href.extract(html.root());
+  auto flat_result = result_tree->flatten();
+
+  for(const auto& m : flat_result)
+    for(const auto& p : m)
+      std::cout << p.second << "\n";
+
+  return EXIT_SUCCESS;
+}
+
