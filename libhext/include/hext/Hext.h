52a020685881b80d84201499ce77937d7abada9e
Tom <tom@universe>
2015-07-13 14:37:16 +0200

    Hext.h: group headers logically, do not include self

--- libhext/include/hext/Hext.h
+++ libhext/include/hext/Hext.h
@@ -2,37 +2,52 @@
 #define HEXT_HEXT_H_INCLUDED
 
 
+// Extraction
+#include "hext/Extractor.h"
+#include "hext/Html.h"
+#include "hext/HtmlTag.h"
+#include "hext/Result.h"
+#include "hext/ResultTree.h"
+#include "hext/Rule.h"
+
+
+// Hext-Parsing
+#include "hext/Parser.h"
+#include "hext/SyntaxError.h"
+
+
+// Captures
+#include "hext/Capture.h"
 #include "hext/AttributeCapture.h"
+#include "hext/FunctionCapture.h"
+#include "hext/CaptureFunction.h"
+#include "hext/Builtins.h"
+
+
+// Matches
+#include "hext/Match.h"
 #include "hext/AttributeCountMatch.h"
 #include "hext/AttributeMatch.h"
-#include "hext/BeginsWithTest.h"
-#include "hext/FunctionCapture.h"
-#include "hext/FunctionMatch.h"
-#include "hext/Builtins.h"
-#include "hext/Capture.h"
-#include "hext/CaptureFunction.h"
 #include "hext/ChildCountMatch.h"
+#include "hext/FunctionMatch.h"
+#include "hext/NegateMatch.h"
+#include "hext/NthChildMatch.h"
+#include "hext/OnlyChildMatch.h"
+#include "hext/TextNodeMatch.h"
+
+
+// ValueTests
+#include "hext/ValueTest.h"
+#include "hext/BeginsWithTest.h"
 #include "hext/ContainsTest.h"
 #include "hext/ContainsWordsTest.h"
 #include "hext/EndsWithTest.h"
 #include "hext/EqualsTest.h"
-#include "hext/Extractor.h"
-#include "hext/Hext.h"
-#include "hext/Html.h"
-#include "hext/HtmlTag.h"
-#include "hext/Match.h"
-#include "hext/NegateMatch.h"
 #include "hext/NegateTest.h"
-#include "hext/NthChildMatch.h"
-#include "hext/OnlyChildMatch.h"
-#include "hext/Parser.h"
 #include "hext/RegexTest.h"
-#include "hext/Result.h"
-#include "hext/ResultTree.h"
-#include "hext/Rule.h"
-#include "hext/SyntaxError.h"
-#include "hext/TextNodeMatch.h"
-#include "hext/ValueTest.h"
+
+
+// Version
 #include "hext/Version.h"
 
 
