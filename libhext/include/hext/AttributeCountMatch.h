50a95bfacbadacd603e6775ef91b77be47f2a3f6
Tom <tom@universe>
2015-07-18 14:52:44 +0200

    AttributeCountMatch: improve documentation

--- libhext/include/hext/AttributeCountMatch.h
+++ libhext/include/hext/AttributeCountMatch.h
@@ -9,16 +9,40 @@
 namespace hext {
 
 
-/// Matches nodes that have a certain amount of attributes.
+/// Matches HTML elements that have a certain amount of HTML attributes.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   GumboNode * no_attrs  = ...; // <hr/>
+///   GumboNode * two_attrs = ...; // <img class="picture" src="mona-lisa.jpg"/>
+///
+///   AttributeCountMatch m_no_attr (/* attribute count */ 0);
+///   AttributeCountMatch m_two_attr(/* attribute count */ 2);
+///
+///   assert(m_no_attr.match(no_attrs));
+///   assert(m_two_attr.match(two_attrs));
+///
+///   assert(!m_two_attr.match(no_attrs));
+///   assert(!m_no_attr.match(two_attrs));
+/// ~~~~~~~~~~~~~
 class AttributeCountMatch : public Match
 {
 public:
+  /// Constructs an AttributeCountMatch that matches HTML elements that have a
+  /// certain amount of HTML attributes.
+  ///
+  /// @param attribute_count:  The amount of HTML attributes a node must have
+  ///                          in order to match.
   explicit AttributeCountMatch(unsigned int attribute_count);
 
-  /// Return true if node has attribute_count amount of attributes.
+  /// Returns true if node has exactly attribute_count amount of HTML
+  /// attributes.
+  ///
+  /// @param node:  A pointer to a GumboNode of type GUMBO_NODE_ELEMENT.
   bool matches(const GumboNode * node) const final;
 
 private:
+  /// The amount of HTML attributes an HTML element must have in order to match.
   unsigned int attribute_count_;
 };
 
