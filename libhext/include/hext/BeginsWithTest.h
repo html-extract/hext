e588faf60ba3de408bd395c541e4e7f76bd9b4bd
Tom <tom@universe>
2015-07-18 15:43:13 +0200

    BeginsWithTest: improve documentation

--- libhext/include/hext/BeginsWithTest.h
+++ libhext/include/hext/BeginsWithTest.h
@@ -9,16 +9,32 @@
 namespace hext {
 
 
-/// Test whether a string begins with a given literal.
+/// Tests whether a string begins with a given literal.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   BeginsWithTest begins("Start");
+///
+///   assert( begins.test("Startles your sleeping ears to hear"));
+///   assert(!begins.test("startles your sleeping ears to hear"));
+///   assert(!begins.test(""));
+/// ~~~~~~~~~~~~~
 class BeginsWithTest : public ValueTest
 {
 public:
+  /// Constructs a BeginsWithTest that succeeds for subjects that begin with
+  /// the given literal.
+  ///
+  /// @param literal:  A string that a subject must begin with.
   explicit BeginsWithTest(std::string literal);
 
-  /// Return true if subject begins with literal.
+  /// Return true if subject begins with the given literal.
+  ///
+  /// @param subject:  The string that is to be tested.
   bool test(const char * subject) const final;
 
 private:
+  /// The literal that must be matched.
   std::string lit_;
 };
 
