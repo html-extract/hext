d4e0ca5fd83b864247f060fd8591e2f374205dcb
Tom <tom@universe>
2015-07-18 17:09:26 +0200

    ChildCountMatch: improve documentation

--- libhext/include/hext/ChildCountMatch.h
+++ libhext/include/hext/ChildCountMatch.h
@@ -9,23 +9,41 @@
 namespace hext {
 
 
-/// A ChildCountMatch is a Match that matches nodes that have a certain
-/// amount of children (exluding text-nodes).
+/// Matches HTML elements that have a certain amount of children of type
+/// element (exluding text nodes, document nodes and others).
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   GumboNode * none = ...; // <div>Text nodes are ignored</div>
+///   GumboNode * two  = ...; // <ul><li>first</li><li>last</li></ul>
+///
+///   ChildCountMatch m_none(/* child count */ 0);
+///   ChildCountMatch m_two (/* child count */ 2);
+///
+///   assert( m_none.matches(none));
+///   assert(!m_none.matches(two));
+///
+///   assert( m_two.matches(two));
+///   assert(!m_two.matches(none));
+/// ~~~~~~~~~~~~~
 class ChildCountMatch : public Match
 {
 public:
-  /// Construct a ChildCountMatch that matches nodes with a child_count amount
-  /// of children. If child_count is negative, child_count will be set to zero.
+  /// Construct a ChildCountMatch that matches HTML elements with a child_count
+  /// amount of children. If child_count is negative, child_count will be set to
+  /// zero.
   explicit ChildCountMatch(int child_count);
 
   /// Return true if node has child_count amount of children.
+  ///
+  /// @param node:  A pointer to a GumboNode of type GUMBO_NODE_ELEMENT.
   bool matches(const GumboNode * node) const final;
 
 private:
-  /// Count amount of children with node type `GUMBO_NODE_ELEMENT` (which
-  /// excludes text-nodes).
+  /// Return amount of node's children that have node type GUMBO_NODE_ELEMENT.
   int count_child_elements(const GumboNode * node) const;
 
+  /// The amount of children an HTML element must have in order to match.
   int child_count_;
 };
 
