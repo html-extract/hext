2cef2e95cc56594f95077459f960c40873b13780
Tom <tom@universe>
2015-07-19 20:56:52 +0200

    NegateMatch: improve documentation

--- libhext/include/hext/NegateMatch.h
+++ libhext/include/hext/NegateMatch.h
@@ -12,22 +12,49 @@
 namespace hext {
 
 
-/// Match if all contained Matches return false for a given GumboNode.
+/// Matches HTML nodes for which every given Match returns false.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   GumboNode * attr_none = ...; // <hr/>
+///   GumboNode * attr_one  = ...; // <h1 class="main-heading"></h1>
+///   GumboNode * attr_two  = ...; // <div id="cart" class="my-cart"></div>
+///
+///   NegateMatch not_one( { std::make_unique<AttributeCountMatch>(1) });
+///   NegateMatch not_none({ std::make_unique<AttributeCountMatch>(0) });
+///
+///   assert(!not_none.matches(attr_none));
+///   assert( not_none.matches(attr_one));
+///   assert( not_none.matches(attr_two));
+///
+///   assert( not_one.matches(attr_none));
+///   assert(!not_one.matches(attr_one));
+///   assert( not_one.matches(attr_two));
+/// ~~~~~~~~~~~~~
 class NegateMatch : public Match
 {
 public:
+  /// Constructs a NegateMatch that matches nodes for which every contained
+  /// Match returns false.
+  ///
+  /// @param v_matches:  An optional vector containing Matches.
   explicit NegateMatch(
     std::vector<std::unique_ptr<Match>>&& v_matches =
       std::vector<std::unique_ptr<Match>>()
   );
 
+  /// Appends a Match.
+  ///
+  /// @param match:  The Match to append.
   void take_match(std::unique_ptr<Match>&& match);
 
-  // Return true if all contained Matches return false.
-  // Return true on empty Matches.
+  /// Returns true if every contained Match returns false for node.
+  ///
+  /// @param node:  A pointer to a GumboNode.
   bool matches(const GumboNode * node) const final;
 
 private:
+  /// A vector containing Matches whose result will be negated.
   std::vector<std::unique_ptr<Match>> matches_;
 };
 
