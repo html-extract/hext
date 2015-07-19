9ccf1ee94e1b0b3b8f292e2e89f9dd363015b8d6
Tom <tom@universe>
2015-07-19 19:50:05 +0200

    FunctionMatch: improve documentation

--- libhext/include/hext/FunctionMatch.h
+++ libhext/include/hext/FunctionMatch.h
@@ -13,22 +13,55 @@
 namespace hext {
 
 
-/// Match if the result of applying a function to an html-node passes
-/// `value_test`.
+/// Matches if the result of applying a given CaptureFunction to an HTML node
+/// passes a ValueTest.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   GumboNode * foo = ...; // <div>This is a foo!</div>
+///   GumboNode * bar = ...; // <div>This is a bar!</div>
+///
+///   FunctionMatch m_foo(
+///     InnerHtmlBuiltin,                     // CaptureFunction
+///     std::unique_ptr<ContainsTest>("foo")  // ValueTest
+///   );
+///   FunctionMatch m_bar(
+///     InnerHtmlBuiltin,                     // CaptureFunction
+///     std::unique_ptr<ContainsTest>("bar")  // ValueTest
+///   );
+///
+///   assert(m_foo.matches(foo));
+///   assert(m_bar.matches(bar));
+///
+///   assert(!m_foo.matches(bar));
+///   assert(!m_bar.matches(foo));
+/// ~~~~~~~~~~~~~
 class FunctionMatch : public Match
 {
 public:
+  /// Constructs a FunctionMatch that matches HTML nodes for which a given
+  /// CaptureFunction returns a result that passes a ValueTest.
+  ///
+  /// @param       func:  The CaptureFunction that will be applied to an HTML
+  ///                     node.
+  /// @param value_test:  The ValueTest that the result of the given
+  ///                     CaptureFunction must pass.
   FunctionMatch(
     CaptureFunction func,
     std::unique_ptr<ValueTest> value_test
   );
 
-  /// Return true if the result of applying `func` to node passes `value_test`,
-  /// false otherwise.
+  /// Returns true if the result of calling the given CaptureFunction with node
+  /// as its first argument passes the given ValueTest.
+  ///
+  /// @param node:  A pointer to a GumboNode.
   bool matches(const GumboNode * node) const final;
 
 private:
+  /// The CaptureFunction that will be applied to an HTML node.
   CaptureFunction func_;
+
+  /// The ValueTest that the result of the given CaptureFunction must pass.
   std::unique_ptr<ValueTest> test_;
 };
 
