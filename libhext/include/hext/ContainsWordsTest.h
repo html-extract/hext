a7058a88016036c147a1cb0d27e95c6d064551a1
Tom <tom@universe>
2015-07-19 18:50:26 +0200

    ContainsWordsTest.h: improve documentation

--- libhext/include/hext/ContainsWordsTest.h
+++ libhext/include/hext/ContainsWordsTest.h
@@ -10,18 +10,40 @@
 namespace hext {
 
 
-/// Test whether another string contains all given words. Word boundaries are
-/// the beginning and end of subject, and spaces.
+/// Tests whether a string contains all given words.
+/// Word boundaries are the beginning and end of subject, and spaces.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   ContainsWordTest cw("foo baz bar");
+///
+///   assert( cw.test("bar baz foo"));
+///   assert( cw.test("bar boing baz foo boing"));
+///   assert(!cw.test("foobaz bar"));
+///   assert(!cw.test("foo bar"));
+/// ~~~~~~~~~~~~~
 class ContainsWordsTest : public ValueTest
 {
 public:
+  /// Constructs a ContainsWordsTest that succeeds for subjects that contain all
+  /// words given in a string.
+  ///
+  /// @param words:  A string that contains space-separated words.
   explicit ContainsWordsTest(std::string words);
+
+  /// Constructs a ContainsWordsTest that succeeds for subjects that contain all
+  /// words given in a vector.
+  ///
+  /// @param words:  A vector of words.
   explicit ContainsWordsTest(std::vector<std::string> words);
 
-  /// Return true if subject contains all words.
+  /// Returns true if subject contains all given words.
+  ///
+  /// @param subject:  The string that is to be tested.
   bool test(const char * subject) const final;
 
 private:
+  /// A vector of words that a subject must contain.
   std::vector<std::string> words_;
 };
 
