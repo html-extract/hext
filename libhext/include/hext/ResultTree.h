df92a9422e6bec9a714d9692e343a25263f123b0
Tom <tom@universe>
2015-07-19 22:45:13 +0200

    ResultTree: improve documentation

--- libhext/include/hext/ResultTree.h
+++ libhext/include/hext/ResultTree.h
@@ -10,36 +10,56 @@
 namespace hext {
 
 
-/// A ResultTree contains the result of matching a Rule to GumboNodes through
-/// `Rule::extract()`.
+/// A self-managing tree containing all values captured from HTML.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   std::string hext_str(
+///     "<a href={href}><img/></a>" // i.e. capture the attribute href from all
+///                                 // anchor elements that have a child with
+///                                 // HtmlTag <img>.
+///   Parser p(hext_str.begin(), hext_str.end());
+///   Rule rule = p.parse();
+///   std::unique_ptr<ResultTree> rt = rule.extract([html]);
+///   Result result = rt->flatten();
+///   // Each element of result is an unordered multimap, each containing one
+///   // string pair.
+/// ~~~~~~~~~~~~~
 class ResultTree
 {
 public:
+  /// Constructs an empty ResultTree.
   ResultTree();
+
   ResultTree(ResultTree&&);
   ResultTree& operator=(ResultTree&&);
   ~ResultTree();
 
-  /// Return a non-owning pointer to the newly created child.
+  /// Appends a new branch.
+  ///
+  /// @returns  A non-owning pointer to the newly created child.
   ResultTree * create_child();
 
-  /// Delete a child branch pointed to by `child`. If `child` is not found
-  /// nothing is done.
+  /// Deletes the child branch pointed to by child.
+  ///
+  /// @param child:  The child branch wich will be deleted. If it is not a child
+  ///                of this instance, the call will have no effect.
   void delete_child(const ResultTree * child);
 
+  /// Sets the captured values for this branch.
+  ///
+  /// @param v:  A vector containing string-pairs.
   void set_values(std::vector<ResultPair> v);
 
-  /// Return read-only access to values.
+  /// Returns read-only access to the values of this tree node.
   const std::vector<ResultPair>& values() const;
 
-  /// Return read-only access to children.
+  /// Returns read-only access to the children of this tree node.
   const std::vector<std::unique_ptr<ResultTree>>& children() const;
 
-  /// Return a vector containing multimaps of string pairs. Each element in the
-  /// vector represents a child of this ResultTree. The multimaps are the result
-  /// of recursively appending each subtree, flattening all values to a single
-  /// map.
-  /// Note: Values from this instance are omitted.
+  /// Returns a vector<ResultMap> containing all values from all children.
+  /// Each element of the vector represents a complete extraction done
+  /// by a Rule.
   Result flatten() const;
 
 private:
