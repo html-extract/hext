5b306fd2a8b030c452442f659eb7d78f81b334a8
Tom <tom@universe>
2015-07-19 21:03:31 +0200

    NegateTest: improve documentation

--- libhext/include/hext/NegateTest.h
+++ libhext/include/hext/NegateTest.h
@@ -9,16 +9,31 @@
 namespace hext {
 
 
-/// Negate the result of another ValueTest.
+/// Negates the result of another ValueTest.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   NegatesTest not_foo(std::make_unique<EqualsTest>("foo"));
+///
+///   assert( not_foo.test("bar"));
+///   assert( not_foo.test("baz"));
+///   assert(!not_foo.test("foo"));
+/// ~~~~~~~~~~~~~
 class NegateTest : public ValueTest
 {
 public:
+  /// Constructs a NegateTest.
+  ///
+  /// @param value_test:  The ValueTest a subject has to fail.
   explicit NegateTest(std::unique_ptr<ValueTest>&& value_test);
 
-  /// Return true if value_test fails for subject.
+  /// Returns true if the given ValueTest fails for subject.
+  ///
+  /// @param subject:  The string that is to be tested.
   bool test(const char * subject) const final;
 
 private:
+  /// The ValueTest a subject has to fail.
   std::unique_ptr<ValueTest> value_test_;
 };
 
