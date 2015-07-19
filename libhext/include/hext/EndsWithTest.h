34c961c335d829f50e482a6d6a940377af5a747c
Tom <tom@universe>
2015-07-19 18:54:51 +0200

    EndsWithTest: improve documentation

--- libhext/include/hext/EndsWithTest.h
+++ libhext/include/hext/EndsWithTest.h
@@ -9,16 +9,32 @@
 namespace hext {
 
 
-/// Test whether another string ends with a given literal.
+/// Tests whether a string ends with a given literal.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   EndsWithTest ends("baz");
+///
+///   assert( ends.test("foobaz"));
+///   assert( ends.test("baz"));
+///   assert(!ends.test("foo"));
+/// ~~~~~~~~~~~~~
 class EndsWithTest : public ValueTest
 {
 public:
+  /// Constructs an EndsWithTest that succeeds for subjects that end with the
+  /// given literal.
+  ///
+  /// @param literal:  A string that a subject must end with.
   explicit EndsWithTest(std::string literal);
 
-  /// Return true if subject ends with literal.
+  /// Returns true if subject ends with the given literal.
+  ///
+  /// @param subject:  The string that is to be tested.
   bool test(const char * subject) const final;
 
 private:
+  /// The literal that must be matched.
   std::string lit_;
 };
 
