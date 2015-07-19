c0ac7cc1d745e4fdf330f23ea2075ebebfeb9961
Tom <tom@universe>
2015-07-19 18:57:50 +0200

    EqualsTest: improve documentation

--- libhext/include/hext/EqualsTest.h
+++ libhext/include/hext/EqualsTest.h
@@ -9,16 +9,32 @@
 namespace hext {
 
 
-/// Test whether another string equals a given literal.
+/// Tests whether a string equals a given literal.
+///
+/// @par Example:
+/// ~~~~~~~~~~~~~
+///   EqualsTest equals("foo");
+///
+///   assert( equals.test("foo"));
+///   assert(!equals.test("foob"));
+///   assert(!equals.test("bfoo"));
+/// ~~~~~~~~~~~~~
 class EqualsTest : public ValueTest
 {
 public:
+  /// Constructs an EqualsTest that succeeds for subjects that equal a given
+  /// literal.
+  ///
+  /// @param literal:  A string that a subject must equal.
   explicit EqualsTest(std::string literal);
 
-  /// Return true if subject equals literal.
+  /// Returns true if subject equals literal.
+  ///
+  /// @param subject:  The string that is to be tested.
   bool test(const char * subject) const final;
 
 private:
+  /// The literal that must be matched.
   std::string lit_;
 };
 
