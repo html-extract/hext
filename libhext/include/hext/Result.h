1cbbb636f7ca4cfe2a26a361e4e038f192cf9d82
Tom <tom@universe>
2015-07-19 22:22:46 +0200

    Result: improve documentation

--- libhext/include/hext/Result.h
+++ libhext/include/hext/Result.h
@@ -1,6 +1,9 @@
 #ifndef HEXT_RESULT_H_INCLUDED
 #define HEXT_RESULT_H_INCLUDED
 
+/// @file
+///   Typedefs for results returned from capturing HTML.
+
 #include <unordered_map>
 #include <vector>
 #include <string>
@@ -10,16 +13,20 @@
 namespace hext {
 
 
-/// A string-pair containg a name and a value.
+/// A string-pair containing a name and a value. A ResultPair is produced by a
+/// Capture.
 typedef std::pair<std::string, std::string> ResultPair;
 
-/// A multimap containing the values produced by capturing.
+
+/// A multimap containing the values produced by capturing. A ResultTree can be
+/// flattened to a vector containing ResultMap.
 typedef std::unordered_multimap<
   ResultPair::first_type,
   ResultPair::second_type
 > ResultMap;
 
-/// A collection of ResultMaps.
+
+/// A vector containing ResultMap. A ResultTree can be flattened to a Result.
 typedef std::vector<ResultMap> Result;
 
 
