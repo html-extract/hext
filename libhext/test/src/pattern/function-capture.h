9f447871cdaa51fc92f41bea6cc6190727e4f0a4
Tom <tom@universe>
2015-07-25 07:42:59 +0200

    libhext-test: add tests for FunctionCapture's documentation examples

--- libhext/test/src/pattern/function-capture.h
+++ libhext/test/src/pattern/function-capture.h
@@ -59,3 +59,64 @@ TEST(Pattern_FunctionCapture, CustomFunction)
   }
 }
 
+TEST(Pattern_FunctionCapture, RegexExampleFromDocumentation)
+{
+  THtml h("<div>Highway 61 revisited</div>");
+
+  {
+    auto result = FunctionCapture(TextBuiltin, "r", boost::regex("\\d+"))
+                      .capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("r", "61"));
+  }
+
+  {
+    auto result = FunctionCapture(TextBuiltin, "r", boost::regex("Highway \\d+"))
+                      .capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("r", "Highway 61"));
+  }
+
+  {
+    auto result = FunctionCapture(TextBuiltin, "r", boost::regex("Highway (\\d+)"))
+                      .capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("r", "61"));
+  }
+
+  {
+    auto result = FunctionCapture(TextBuiltin, "r", boost::regex("\\w+"))
+                      .capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("r", "Highway"));
+  }
+
+  {
+    auto result = FunctionCapture(TextBuiltin, "r", boost::regex("(\\w+) (\\d+)"))
+                      .capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("r", "Highway"));
+  }
+}
+
+TEST(Pattern_FunctionCapture, ExampleFromDocumentation)
+{
+  {
+    THtml h("<div>5 reasons why foo is better than bar</div>");
+    FunctionCapture text(TextBuiltin, "text");
+    auto result = text.capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("text", "5 reasons why foo is better than bar"));
+  }
+
+  {
+    THtml h("<div>The result is 25cm.</div>");
+    FunctionCapture centimeters(InnerHtmlBuiltin,
+                                "centimeters",
+                                boost::regex("(\\d+)cm"));
+    auto result = centimeters.capture(h.first());
+    ASSERT_TRUE(result);
+    EXPECT_EQ(*result, ResultPair("centimeters", "25"));
+  }
+}
+
