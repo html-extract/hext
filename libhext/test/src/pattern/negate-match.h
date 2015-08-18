c52203e4b0db4eb463cc6b98a2dff99e33c271b5
Tom <tom@universe>
2015-08-18 23:19:35 +0200

    test: NegateMatch method take_match was renamed to append_match

--- libhext/test/src/pattern/negate-match.h
+++ libhext/test/src/pattern/negate-match.h
@@ -3,7 +3,7 @@
 TEST(Pattern_NegateMatch, Matches)
 {
   NegateMatch no_href;
-  no_href.take_match(std::make_unique<AttributeMatch>("href"));
+  no_href.append_match(std::make_unique<AttributeMatch>("href"));
 
   THtml h("<a class='dummy'></a>");
   EXPECT_TRUE(no_href.matches(h.first()));
@@ -13,7 +13,7 @@ TEST(Pattern_NegateMatch, Matches)
 TEST(Pattern_NegateMatch, Fails)
 {
   NegateMatch no_href;
-  no_href.take_match(std::make_unique<AttributeMatch>("href"));
+  no_href.append_match(std::make_unique<AttributeMatch>("href"));
 
   THtml h("<a href='dummy'></a>");
   EXPECT_FALSE(no_href.matches(h.first()));
