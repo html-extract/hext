082d5afc9f3a4f61bac21d69f0b3daa63d7b9ab9
Tom <tom@universe>
2015-08-18 23:25:10 +0200

    test: Rule: fix api changes

--- libhext/test/src/rule/rule.h
+++ libhext/test/src/rule/rule.h
@@ -4,12 +4,12 @@
 TEST(Rule_Rule, ExampleFromDocumentation)
 {
   Rule anchor(HtmlTag::A);
-  anchor.add_match<AttributeMatch>("href")
-        .add_capture<AttributeCapture>("href", "link");
+  anchor.append_match<AttributeMatch>("href")
+        .append_capture<AttributeCapture>("href", "link");
   {
     Rule img(HtmlTag::IMG);
-    img.add_capture<AttributeCapture>("src", "img");
-    anchor.take_child(std::move(img));
+    img.append_capture<AttributeCapture>("src", "img");
+    anchor.append_child(std::move(img));
   }
 
   // <a href={link}><img src={img}/></a>
@@ -20,8 +20,7 @@ TEST(Rule_Rule, ExampleFromDocumentation)
       "<div><a href='/carol'><img src='carol.jpg'/></a></div>"
     "</body></html>");
 
-  std::unique_ptr<ResultTree> rt = anchor.extract(html.root());
-  hext::Result result = rt->flatten();
+  Result result = anchor.extract(html);
 
   Result expected = {
     {
@@ -46,13 +45,10 @@ TEST(Rule_Rule, SettersGetters)
   Rule r;
   EXPECT_EQ(r.get_tag(), HtmlTag::ANY);
   EXPECT_EQ(r.is_optional(), false);
-  EXPECT_EQ(r.is_path(), false);
 
   r.set_tag(HtmlTag::SPAN);
   r.set_optional(true);
-  r.set_path(true);
   EXPECT_EQ(r.get_tag(), HtmlTag::SPAN);
   EXPECT_EQ(r.is_optional(), true);
-  EXPECT_EQ(r.is_path(), true);
 }
 
