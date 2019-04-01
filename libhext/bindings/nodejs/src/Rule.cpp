717e07b0ac448575d71b798f6d86406a4c6b55c8
Tom <git@thomastrapp.com>
2019-04-01 18:26:03 +0200

    libhext/bindings/nodejs: add support for nodejs v10/v11

--- libhext/bindings/nodejs/src/Rule.cpp
+++ libhext/bindings/nodejs/src/Rule.cpp
@@ -83,7 +83,7 @@ NAN_METHOD(Rule::New)
     const int argc = 1;
     v8::Local<v8::Value> argv[argc] = {info[0]};
     v8::Local<v8::Function> cons = Nan::New(constructor);
-    info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+    info.GetReturnValue().Set(Nan::NewInstance(cons, argc, argv).ToLocalChecked());
   }
 }
 
