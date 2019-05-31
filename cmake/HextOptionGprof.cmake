0396a6a079960a6e665459ce130f2fbf964561de
Tom <git@thomastrapp.com>
2019-05-31 15:08:40 +0200

    htmlext: cmake remove HextOptionGprof

--- cmake/HextOptionGprof.cmake
+++
@@ -1,9 +0,0 @@
-# Option GPROF, default OFF
-# GNU gprof build
-
-option(GPROF "gprof build" OFF)
-if(GPROF)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pg") 
-  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -pg")
-endif()
-
