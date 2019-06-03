3a233bbf72c30f0fa72d165270dc552dedda51ac
Tom <git@thomastrapp.com>
2019-06-03 19:04:01 +0200

    libhext: cmake: depend on target Gumbo::Gumbo
    FindGumbo.cmake: add imported target Gumbo::Gumbo.

--- cmake/FindGumbo.cmake
+++ cmake/FindGumbo.cmake
@@ -1,9 +1,15 @@
-# http://www.cmake.org/cmake/help/v3.0/module/FindPackageHandleStandardArgs.html
-
 include(FindPackageHandleStandardArgs)
 
 find_path(Gumbo_INCLUDE_DIR gumbo.h)
 find_library(Gumbo_LIBRARY NAMES gumbo)
 
-find_package_handle_standard_args(Gumbo DEFAULT_MSG Gumbo_LIBRARY Gumbo_INCLUDE_DIR)
+find_package_handle_standard_args(Gumbo REQUIRED_VARS Gumbo_LIBRARY Gumbo_INCLUDE_DIR)
+
+if(Gumbo_FOUND AND NOT TARGET Gumbo::Gumbo)
+  add_library(Gumbo::Gumbo UNKNOWN IMPORTED)
+  set_target_properties(Gumbo::Gumbo
+    PROPERTIES
+      IMPORTED_LOCATION "${Gumbo_LIBRARY}"
+      INTERFACE_INCLUDE_DIRECTORIES "${Gumbo_INCLUDE_DIR}")
+endif()
 
