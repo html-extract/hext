4ec0f0f6274612882e4b01b9a6e6bca60adc0399
Tom <git@thomastrapp.com>
2019-06-03 18:25:57 +0200

    htmlext: cmake: depend on target RapidJSON
    FindRapidJSON.cmake: add imported target RapidJSON::RapidJSON.

--- cmake/FindRapidJSON.cmake
+++ cmake/FindRapidJSON.cmake
@@ -1,8 +1,15 @@
-# http://www.cmake.org/cmake/help/v3.0/module/FindPackageHandleStandardArgs.html
-
 include(FindPackageHandleStandardArgs)
 
-find_path(RapidJSON_INCLUDE_DIRS rapidjson/rapidjson.h)
+find_path(RapidJSON_INCLUDE_DIR rapidjson/rapidjson.h)
+mark_as_advanced(RapidJSON_INCLUDE_DIR)
 
-find_package_handle_standard_args(RapidJSON DEFAULT_MSG RapidJSON_INCLUDE_DIRS)
+find_package_handle_standard_args(RapidJSON
+  REQUIRED_VARS RapidJSON_INCLUDE_DIR)
+
+if(RapidJSON_FOUND AND NOT TARGET RapidJSON::RapidJSON)
+  add_library(RapidJSON::RapidJSON INTERFACE IMPORTED)
+  set_target_properties(RapidJSON::RapidJSON
+    PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${RapidJSON_INCLUDE_DIR}")
+endif()
 
