b9d7947ffc96f99d98ec7f0a20f15ac846288ea0
Tom <git@thomastrapp.com>
2021-12-17 17:43:21 +0100

    bump version to v1.0.1

--- scripts/github-actions/pypi/assets/setup.py
+++ scripts/github-actions/pypi/assets/setup.py
@@ -31,7 +31,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 setup(
     name='hext',
     package_data={'hext': ['_hext.so', 'gumbo.license', 'rapidjson.license']},
-    version='1.0.0',
+    version='1.0.1',
     description="A module and command-line utility to extract structured data from HTML",
     long_description=long_description,
     long_description_content_type="text/markdown",
