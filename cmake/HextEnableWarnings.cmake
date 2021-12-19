2c8230806fed37781f4a526578a0d2901b9f8a1d
Tom <git@thomastrapp.com>
2021-12-19 17:42:55 +0100

    cmake: add gcc warnings newly added in gcc-{8,9,10,11}

--- cmake/HextEnableWarnings.cmake
+++ cmake/HextEnableWarnings.cmake
@@ -2,7 +2,7 @@ function(hext_enable_warnings_gnu)
   target_compile_options(
     ${ARGN}
     "-Wall"
-    "-Wcast-align"
+    "-Wcast-align=strict"
     "-Wcast-qual"
     "-Wconversion"
     "-Wctor-dtor-privacy"
@@ -37,15 +37,28 @@ function(hext_enable_warnings_gnu)
     "-Wwrite-strings"
     "-pedantic"
     "-pedantic-errors"
+    "-Wformat-overflow=2"
+    "-Wformat-truncation=2"
+    "-Wreturn-local-addr"
+    "-Warith-conversion"
+    "-Wstringop-overflow=4"
+    "-Wmismatched-tags"
+    "-Wredundant-tags"
+    "-Wsuggest-override"
+    "-Wctad-maybe-unsupported"
+    "-Wdeprecated-enum-enum-conversion"
+    "-Wdeprecated-enum-float-conversion"
+    "-Wunsafe-loop-optimizations"
     "-Wswitch-default"
     "-Wswitch-enum"
     # Unused switches:
     # "-Wpadded": also warns when it's impossible to shrink padding
+    # Notes:
     # "-Wunsafe-loop-optimizations":
     #   GCC7 will emit warnings when using range-based for loops:
     #     "warning: missed loop optimization, the loop counter may overflow"
     #   Since it's only complaining about a missed optimization, this warning
-    #   can safely be disabled.
+    #   can safely be ignored.
   )
 endfunction()
 
