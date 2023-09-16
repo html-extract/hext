c611bd8281810d44c3211965330eefa44134d50f
Tom <git@thomastrapp.com>
2023-09-16 13:58:06 +0200

    htmlext: ProgramOptions: add missing include cstdint

--- htmlext/htmlext/ProgramOptions.cpp
+++ htmlext/htmlext/ProgramOptions.cpp
@@ -1,4 +1,4 @@
-// Copyright 2015-2021 Thomas Trapp
+// Copyright 2015-2023 Thomas Trapp
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
 
 #include "htmlext/ProgramOptions.h"
 
+#include <cstdint>
 #ifdef _WIN32
 #include <io.h>
 #define isatty _isatty
